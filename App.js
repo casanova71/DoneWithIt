404c4ad51708f15f190cc8de7a3c20268024a39e
Archit <archi4play@gmail.com>
2024-08-24 15:01:24 +0530

    Commit

--- App.js
+++ App.js
@@ -1,19 +1,60 @@
 
-import {StyleSheet, View, SafeAreaView, StatusBar,Platform} from 'react-native';
-import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
+// import {StyleSheet, View, SafeAreaView, StatusBar,Platform} from 'react-native';
+import {View} from 'react-native';
+import React from 'react';
+import WelcomeScreen from './app/screen/WelcomeScreen';
+// import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 export default function App() {
-  console.log(useDeviceOrientation());
-  return (
-    <SafeAreaView style={[styles.container]}>
-<View style={{
-backgroundColor: "dodgerblue",
-height: '30%',
-width: '100%',
+return <WelcomeScreen />;
+}
 
+/* <View style={{
+backgroundColor: "#fff",
+flex: 1,
+flexDirection: 'row', //horizonatal
+justifyContent: 'center', //main 
+alignItems: 'center', //secondary
+alignContent: 'center',
+// flexWrap: 'wrap',
 }}>
+<View  style={{
+  backgroundColor: "dodgerblue",
+  // flexBasis: 100, // width pr height
+  // flex: 1,
+ width: 100,
+  height: 100,
+  // flex: -1,
+
+}}/>
+<View  style={{
+  backgroundColor: "gold",
+  width: 100, 
+  height: 100,
+  // top:20,
+  // left:20, 
+  // position: 'absolute',
+}}/>
+<View  style={{
+  backgroundColor: "tomato",
+  width: 100, height: 100,
+}}/>
 
 
-</View>
+</View> */
+
+
+
+
+// //     <SafeAreaView style={[styles.container]}>
+// // <View style={{
+// // backgroundColor: "dodgerblue",
+// // height: '30%',
+// // width: '100%',
+
+// }}>
+
+
+
 
       {/* <Button 
       color="black"
@@ -21,17 +62,17 @@ width: '100%',
          console.log("Hello world")
       }
       /> */}
-        </SafeAreaView>
+//         </SafeAreaView>
         
-  );
-}
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-   paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "white",
+//    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
 
    
-  },
-});
+//   },
+// });
